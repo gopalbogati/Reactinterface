6b444817f5053923262949e0aa79fd32f505e396
Gopal Bogati <gopal2736@gmail.com>
2020-11-15 12:34:20 +1100

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+
+import '../css/App.css';
+
+function App() {
+  return (
+    <main className="page bg-white" id="petratings">
+    <div className="container">
+      <div className="row">
+        <div className="col-md-12 bg-white">
+          <div className="container">
+            <div>Add Appointment</div>
+            <div>Search Appointment</div>
+            <div>list Appointment</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+  );
+}
+
+export default App;
